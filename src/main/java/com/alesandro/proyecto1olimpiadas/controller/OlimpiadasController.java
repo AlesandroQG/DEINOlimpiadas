da0cf2a313951d6285ce4c4241b36e13a95549a5
Alesandro <116791603+AlesandroQuiros@users.noreply.github.com>
2024-10-17 19:31:45 +0200

    Creación daos

--- src/main/java/com/alesandro/proyecto1olimpiadas/controller/OlimpiadasController.java
+++ src/main/java/com/alesandro/proyecto1olimpiadas/controller/OlimpiadasController.java
@@ -2,6 +2,7 @@ package com.alesandro.proyecto1olimpiadas.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -21,4 +22,31 @@ public class OlimpiadasController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //
     }
+
+    /**
+     * Función que muestra un mensaje de alerta al usuario
+     *
+     * @param texto contenido de la alerta
+     */
+    public void alerta(String texto) {
+        Alert alerta = new Alert(Alert.AlertType.ERROR);
+        alerta.setHeaderText(null);
+        alerta.setTitle("ERROR");
+        alerta.setContentText(texto);
+        alerta.showAndWait();
+    }
+
+    /**
+     * Función que muestra un mensaje de confirmación al usuario
+     *
+     * @param texto contenido del mensaje
+     */
+    public void confirmacion(String texto) {
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setHeaderText(null);
+        alerta.setTitle("Info");
+        alerta.setContentText(texto);
+        alerta.showAndWait();
+    }
+
 }
